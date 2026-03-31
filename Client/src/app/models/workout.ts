ba6202dca75094753be4cbc4dbfce4140fb975b3
Daniel <ferndani00@gmail.com>
2026-03-31 21:06:40 +0200

    added model

---
+++ Client/src/app/models/workout.ts
@@ -0,0 +1,21 @@
+export class Workout {
+  id: number = 0;
+  name: string = "";
+  type: string = "";
+  sport: string = "";
+  date: Date = new Date();
+  time: number = 0;
+  calories: number = 0;
+  intensity: number = 0;
+
+  constructor(id: number, name: string, type: string, sport: string, date: Date, time: number, calories: number, intensity: number) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.sport = sport;
+    this.date = date;
+    this.time = time;
+    this.calories = calories;
+    this. intensity = intensity;
+  }
+}
