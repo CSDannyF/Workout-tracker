380b009a01fc92299561fa48bc60c72c6819e3e9
Daniel <ferndani00@gmail.com>
2026-04-01 20:59:51 +0200

    Added navigation and routerlinks

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WorkoutList } from './components/workout-list/workout-list';
+import { WorkoutForm } from './components/workout-form/workout-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "workouts", component: WorkoutList },
+  { path: "workouts/add", component: WorkoutForm },
+  { path: "", redirectTo: "workouts", pathMatch: "full" }
+];
