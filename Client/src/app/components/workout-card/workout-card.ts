3f613a7a87b2ef1596286f7b93531c33acb68e17
Daniel <ferndani00@gmail.com>
2026-04-02 19:12:49 +0200

    added styling and relativedatepipe

--- Client/src/app/components/workout-card/workout-card.ts
+++ Client/src/app/components/workout-card/workout-card.ts
@@ -1,14 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { Workout } from '../../models/workout';
+import { MatCardModule } from '@angular/material/card'
+
+import { RelativeDatePipe } from '../../pipes/relative-date-pipe';
 
 @Component({
   selector: 'app-workout-card',
-  imports: [],
+  imports: [MatCardModule, RelativeDatePipe],
   templateUrl: './workout-card.html',
   styleUrl: './workout-card.css',
 })
 export class WorkoutCard {
   @Input()
   workout!: Workout;
-  
 }
