9c45dfbf3921f1072746e9fd3b9075c40024ace1
Daniel <ferndani00@gmail.com>
2026-04-06 21:42:27 +0200

    dropdown sports added

--- Client/src/app/components/workout-form/workout-form.ts
+++ Client/src/app/components/workout-form/workout-form.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Workout } from '../../models/workout';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -7,26 +7,30 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button'
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-
+import {MatSelectModule} from '@angular/material/select';
+import { MatAnchor } from "@angular/material/button";
 
 import { WorkoutService } from '../../services/workout-service';
-import { MatAnchor } from "@angular/material/button";
+import { SportService } from '../../services/sport-service';
+import { Sport } from '../../models/sport';
 
 @Component({
   selector: 'app-workout-form',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatAnchor, MatButtonModule, MatDatepickerModule, MatNativeDateModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatAnchor, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatSelectModule],
   templateUrl: './workout-form.html',
   styleUrl: './workout-form.css',
 })
-export class WorkoutForm {  
+export class WorkoutForm implements OnInit {  
 
   // met Partial kan ik een leeg object aanmaken
   workout: Partial<Workout> = {};
+  sports: Sport[] = [];
 
-  @Output()
-  addWorkout: EventEmitter<Workout> = new EventEmitter<Workout>;
+  constructor(private workoutService: WorkoutService, private sportService: SportService, private router: Router) {}
 
-  constructor(private workoutService: WorkoutService, private router: Router) {}
+  async ngOnInit(): Promise<void> {
+    this.sports = await this.sportService.getSports();
+  }
 
   async onSubmit() {
     let newWorkout = new Workout(
