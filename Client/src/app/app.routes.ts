fb0989259673727ce88a826d577d9440e5b5d64a
Daniel <ferndani00@gmail.com>
2026-04-08 23:15:57 +0200

    added profile and edit profile

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { WorkoutList } from './components/workout-list/workout-list';
 import { WorkoutForm } from './components/workout-form/workout-form';
+import { Profile } from './components/profile/profile';
 
 export const routes: Routes = [
   { path: "workouts", component: WorkoutList },
   { path: "workouts/add", component: WorkoutForm },
-  { path: "workouts", component: WorkoutList, runGuardsAndResolvers: "always" }
+  { path: "profile", component: Profile }
 ];
--- Client/src/app/components/workout-form/workout-form.ts
+++ Client/src/app/components/workout-form/workout-form.ts
@@ -7,7 +7,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button'
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule} from '@angular/material/select';
 import { MatAnchor } from "@angular/material/button";
 
 import { WorkoutService } from '../../services/workout-service';
